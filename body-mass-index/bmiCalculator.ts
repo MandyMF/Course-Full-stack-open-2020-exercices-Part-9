89796767489e2657d919e196b7afad55aa9cd829
MandyMF <mandy@fonoma.com>
2020-11-30 17:11:35 -0500

    Exercise 9.5

--- body-mass-index/bmiCalculator.ts
+++ body-mass-index/bmiCalculator.ts
@@ -33,3 +33,5 @@ try {
 catch (e) {
   console.log('Error, something bad happened, message: ', e.message);
 }
+
+export default calculateBmi
--- body-mass-index/index.ts
+++ body-mass-index/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator'
 
 const app = express()
 
@@ -6,9 +7,24 @@ app.get('/hello', (_req, res)=>{
   res.send('Hello Full Stack!')
 })
 
+app.get('/bmi', (req, res)=>{
+
+  if(!Number(req.query.height) || !Number(req.query.weight))
+    return res.json({
+      error: 'malformatted parameters'
+    })
+
+  return res.json(
+  {
+    weight: Number(req.query.weight),
+    height: Number(req.query.height),
+    bmi: calculateBmi( Number(req.query.height),Number(req.query.weight))
+  }
+  )
+})
+
 const PORT = 3002
 
 app.listen(PORT, ()=> {
   console.log(`Server running at Port: ${PORT}`)
 })
-
