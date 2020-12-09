728c3c8a32349eb8d1d76f25bae8185128fb73af
MandyMF <mandy@fonoma.com>
2020-12-09 03:30:58 -0500

    Exercise 9.17

---
+++ patientor-frontend/src/PatientInfoPage/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {useParams} from 'react-router-dom';
+import {useStateValue} from '../state/state'; 
+import axios from 'axios';
+import { apiBaseUrl } from "../constants";
+import { Icon } from "semantic-ui-react";
+
+const PatientInfoPage: React.FC = () => {
+  const { id } = useParams<{id: string}>();
+  const [{ patient }, dispatch] = useStateValue();
+
+  React.useEffect(() => {
+    const getPatientById = async () =>{
+      try {
+        const {data} = await axios.get(`${apiBaseUrl}/patients/${id}`);
+        dispatch({type: "SET_PATIENT", payload: data});
+      } 
+      catch (error) {
+        console.log(error);
+      }
+    };
+
+    if(!patient || patient?.id !== id){
+      getPatientById();
+    }
+  },[id, patient, dispatch]);
+
+  const IconByGender = () =>{
+    if(patient?.gender === "male"){
+      return <Icon name="mars"></Icon>;
+    }
+    else if(patient?.gender === "female"){
+      return <Icon name="venus"></Icon>;
+    }
+    else{
+      return <Icon name="genderless"></Icon>;
+    }
+  };
+
+  return (
+  <section>
+    <h2>
+      {patient?.name} {IconByGender()}
+    </h2>
+    {patient?.ssn ? <div><span>ssn: {patient?.ssn}</span></div>: <></>}
+    <div><span>occupation: {patient?.occupation}</span></div>
+  </section>
+  );
+};
+
+export default PatientInfoPage;
